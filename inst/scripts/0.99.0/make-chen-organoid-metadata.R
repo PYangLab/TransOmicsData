eedcc2c51b3a42b43df34039712ae87bae0c18d8
Carissa Chen <carissachen97@gmail.com>
2024-04-08 16:01:57 +1000

    add scRNA-seq data to chen-organoid

--- inst/scripts/0.99.0/make-chen-organoid-metadata.R
+++ inst/scripts/0.99.0/make-chen-organoid-metadata.R
@@ -1,19 +1,19 @@
 meta <- data.frame(
-  Title =  sprintf("Chen organoid %s", c("phosphoproteome", "proteome", "transcriptome")),
+  Title =  sprintf("Chen organoid %s", c("phosphoproteome", "proteome", "transcriptome", "sctranscriptome")),
   Description = "developmental timecourse of the early neural organoid up to 8 days",
   BiocVersion="3.19",
   Genome="hg38",
-  SourceType = "RData",
-  SourceUrl = c("https://proteomecentral.proteomexchange.org/cgi/GetDataset?ID=PXD047423", "https://proteomecentral.proteomexchange.org/cgi/GetDataset?ID=PXD047423", "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE248901"),
+  SourceType = c("RData", "RData", "RData", "RDS"),
+  SourceUrl = c("https://proteomecentral.proteomexchange.org/cgi/GetDataset?ID=PXD047423", "https://proteomecentral.proteomexchange.org/cgi/GetDataset?ID=PXD047423", "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE248901", "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE248901"),
   SourceVersion = '0.0',
   Species = "Homo sapiens",
   TaxonomyId=9606,
   Coordinate_1_based="NA",
-  DataProvider=c("PRIDE", "PRIDE", "NCBI"),
+  DataProvider=c("PRIDE", "PRIDE", "NCBI", "NCBI"),
   Maintainer="Di Xiao <d.xiao@sydney.edu.au>",
   RDataClass= "SummarizedExperiment",
   DispatchClass="Rds",
-  RDataPath = file.path("TransOmicsData", "0.99.0", "chen-organoid", c("phosphoproteome.rds", "proteome.rds", "transcriptome.rds")),
+  RDataPath = file.path("TransOmicsData", "0.99.0", "chen-organoid", c("phosphoproteome.rds", "proteome.rds", "transcriptome.rds", "sctranscriptome.rds")),
   stringsAsFactors = FALSE
 )
 
--- inst/scripts/0.99.0/make-chen-organoid.R
+++ inst/scripts/0.99.0/make-chen-organoid.R
@@ -7,12 +7,15 @@ library(S4Vectors)
 library(here)
 
 load(file=here("data", "allomics_processed_ratios.RData"))
+scRNA_counts = readRDS(file=here("data", "scRNA_count_matrix.rds"))
+scRNA_meta = read.delim2(file=here("data", "scRNA_metadata.txt"))
 
 # create metadata file
 meta.masspec = DataFrame(day = sapply(strsplit(colnames(organoid.phosphoproteome.ratio), "_"), "[[",1),
           replicate = sapply(strsplit(colnames(organoid.phosphoproteome.ratio), "_"), "[[", 2))
 meta.rna = DataFrame(day = sapply(strsplit(colnames(organoid.transcriptome.ratio), "_"), "[[",1),
                      replicate = sapply(strsplit(colnames(organoid.transcriptome.ratio), "_"), "[[", 2))
+meta.scrna = DataFrame(scRNA_meta)
 
 # convert to summarized experiment object
 organoid.phosphoproteome.se = SummarizedExperiment(assays=list(ratios = organoid.phosphoproteome.ratio),
@@ -21,8 +24,10 @@ organoid.proteome.se = SummarizedExperiment(assays=list(ratios = organoid.proteo
                                                    colData = meta.masspec)
 organoid.transcriptome.se = SummarizedExperiment(assays=list(ratios = organoid.transcriptome.ratio),
                                             colData = meta.rna)
+organoid.sctranscriptome.se = SummarizedExperiment(assays=list(counts = scRNA_counts[, rownames(meta.scrna)]), colData = meta.scrna)
 
 # save individually as an rds file
 saveRDS(organoid.phosphoproteome.se, here("data", "0.99.0", "chen-organoid", "phosphoproteome.rds"))
 saveRDS(organoid.proteome.se, here("data", "0.99.0", "chen-organoid", "proteome.rds"))
 saveRDS(organoid.transcriptome.se, here("data", "0.99.0", "chen-organoid", "transcriptome.rds"))
+saveRDS(organoid.sctranscriptome.se, here("data", "0.99.0", "chen-organoid", "sctranscriptome.rds"))
