55a4742c96182c354d1fe3bf27372185617ddf51
Carissa Chen <carissachen97@gmail.com>
2024-05-12 23:45:27 +1000

    edit manifest.txt

--- inst/scripts/make-manifest.R
+++ inst/scripts/make-manifest.R
@@ -2,10 +2,13 @@ meta <- data.frame(
   Title = c("chen-organoid", "xiao-myogenesis", "yang-esc"),
   Description = c("neural organoid differentiation", "C2C12 myogenesis differentiation", "ESC to epiLC differentiation"),
   Omics = c("phosphoproteome, proteome, transcriptome, single-cell transcriptome", "phosphoproteome, proteome", "epigenome, phosphoproteome, proteome, transcriptome"),
-  Species = c("human", "mouse", "human"),
+  Species = c("human", "mouse", "mouse"),
   RDataPath = c(file.path("TransOmicsData", "0.99.0", "chen-organoid"),
                 file.path("TransOmicsData", "0.99.0", "xiao-myogenesis"),
                 file.path("TransOmicsData", "0.99.0", "yang-esc")),
+  Reference = c("Chen et al. Trans-omic Profiling Uncovers Molecular Controls of the Early Human Cerebral Organoid Formation. 2024",
+  "Xiao et al. Time-resolved phosphoproteome and proteome analysis reveals kinase signaling on master transcription factors during myogenesis. 2022",
+  "Yang et al. Multi-omic Profiling Reveals Dynamics of the Phased Progression of Pluripotency. 2019"),        
   stringsAsFactors = FALSE
 )
 
